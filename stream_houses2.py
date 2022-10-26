9756d90de96260d5b1b47e5b4726fb5ac3eed27e
tialaron <30555530+tialaron@users.noreply.github.com>
2022-10-26 23:28:57 +0300

    Update stream_houses2.py

--- stream_houses2.py
+++ stream_houses2.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 from tensorflow.keras.models import load_model
+from tensorflow.keras import utils              #Используем для to_categorical
 
 pictures_path = '/app/streamlit_houses2/pictures/'
 model01 = load_model('/app/streamlit_houses2/housemodel1.h5')
